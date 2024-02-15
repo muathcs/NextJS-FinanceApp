00239ab9c16baad7b4d598aa67bab955fa850f83
Muath Khalifa <muath.khalifa@yahoo.com>
2024-02-15 02:55:19 +0000

    layout section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,30 @@
 import Navigation from "@/components/Navigation";
 import Image from "next/image";
 import { currencyFormatter } from "@/lib/utils";
+import ExpenseItem from "@/components/ExpenseItem";
+
+const DUMY_DATA = [
+  {
+    color: "red",
+    text: "Gym",
+    amount: 20,
+  },
+  {
+    color: "green",
+    text: "electricity",
+    amount: 210,
+  },
+  {
+    color: "orange",
+    text: "Netflix",
+    amount: 220,
+  },
+  {
+    color: "brown",
+    text: "Films",
+    amount: 12,
+  },
+];
 export default function Home() {
   return (
     <main className="container max-w-2xl px-6 mx-auto ">
@@ -12,6 +36,21 @@ export default function Home() {
       <section className="flex items-center gap-2 py-3">
         <button className="btn btn-primary">+ Expenses</button>
         <button className="btn btn-primary-outline">+ Income</button>
+      </section>
+
+      {/* Expenses */}
+
+      <section className="py-6">
+        <h3 className="text-2xl">My Expenses</h3>
+        <div className="flex flex-col gap-4 mt-6">
+          {DUMY_DATA.map((item) => (
+            <ExpenseItem
+              color={item.color}
+              title={item.text}
+              amount={item.amount}
+            />
+          ))}
+        </div>
       </section>
     </main>
   );
